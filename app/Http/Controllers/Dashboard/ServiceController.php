b5bab4acfeeba8d423ddda26663f1a0b89fe5afb
MadeReihan <reihanmade@gmail.com>
2022-03-22 20:42:39 +0700

    service

--- app/Http/Controllers/Dashboard/ServiceController.php
+++ app/Http/Controllers/Dashboard/ServiceController.php
@@ -3,18 +3,45 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+
+use App\Http\Requests\Dashboard\Service\StoreServiceRequest;
+use App\Http\Requests\Dashboard\Service\UpdateServiceRequest;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+
+use Symfony\Component\HttpFoundation\Response;
+
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\AdvantageService;
+use App\Models\Tagline;
+use App\Models\AdvantageUser;
+use App\Models\Service;
+use App\Models\ThumbnailService;
+use App\Models\Order;
+use App\Models\User;
 
 class ServiceController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        return view('pages.dashboard.service.index');
+    {   
+        $services = Service::where('user_id', Auth::user()->id)->orderBy('created_at','desc')->get();
+
+
+        return view('pages.dashboard.service.index', compact('serivces'));
     }
 
     /**
@@ -33,9 +60,55 @@ class ServiceController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreServiceRequest $request)
     {
-        //
+        $data = $request->all();
+        $data['users_id'] = Auth::user()->id; 
+
+        // add to service
+        $service = Service::create($data);
+
+        // add to advantage
+        foreach($data['advantage-service'] as $key =>$value){
+            $advantage_service = new AdvantageService;
+            $advantage_service->service_id = $service->id;
+            $advantage_service->advantage_id = $value;
+            $advantage_service->save();
+        }
+
+        // add to advantage user
+        foreach($data['advantage-user'] as $key =>$value){
+            $advantage_user = new AdvantageUser;
+            $advantage_user->service_id = $service->id;
+            $advantage_user->advantage_id = $value;
+            $advantage_user->save();
+        }
+
+        // add to thumbnail service
+        if($request->hasfile('thumbnail')){
+            foreach ($request->file('thumbnail') as $file) {
+                $path = $file->store(
+                    'assets/service/thumbnail', 'public'
+                );
+
+                $thumbnail_service = new ThumbnailService;
+                $thumbnail_service->service_id = $service['id'];
+                $thumbnail_service->thumbnail = $path;
+                $thumbnail_service->save();
+
+            }
+        }
+
+        // add to tagline
+        foreach ($data['tagline'] as $key => $value) {
+            $tagline = new Tagline;
+            $tagline->service_id = $service->id;
+            $tagline->tagline = $value;
+            $tagline->save();
+        }
+
+        toast()->success('Service berhasil ditambahkan');
+        return redirect()->route('member.service.index');
     }
 
     /**
